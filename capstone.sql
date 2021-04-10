46d0d296346f3109a92f3ceeb595cfdf0f44342a
Yam Zheng Lim <yamzhenglim0427@gmail.com>
2021-04-10 17:18:32 +0800

    Database major update

--- capstone.sql
+++ capstone.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 5.1.0
+-- version 5.0.2
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Apr 10, 2021 at 05:33 AM
--- Server version: 10.4.18-MariaDB
--- PHP Version: 8.0.3
+-- Generation Time: Apr 10, 2021 at 11:17 AM
+-- Server version: 10.4.14-MariaDB
+-- PHP Version: 7.4.10
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -28,39 +28,21 @@ SET time_zone = "+00:00";
 --
 
 CREATE TABLE `bookingform` (
-  `id` int(11) NOT NULL,
-  `customer_name` text NOT NULL,
-  `contact` int(11) NOT NULL,
-  `datentime` datetime NOT NULL,
-  `people` int(11) NOT NULL
+  `booking_id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
+  `booking_fullname` text NOT NULL,
+  `booking_contact` varchar(11) NOT NULL,
+  `booking_date` date NOT NULL,
+  `booking_time` time NOT NULL,
+  `numberPerson` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 --
 -- Dumping data for table `bookingform`
 --
 
-INSERT INTO `bookingform` (`id`, `customer_name`, `contact`, `datentime`, `people`) VALUES
-(1, '', 0, '0000-00-00 00:00:00', 0),
-(2, '', 0, '0000-00-00 00:00:00', 0),
-(3, '', 0, '0000-00-00 00:00:00', 0),
-(4, '', 0, '0000-00-00 00:00:00', 0),
-(5, '', 0, '0000-00-00 00:00:00', 0),
-(6, '', 0, '0000-00-00 00:00:00', 0),
-(7, '', 0, '0000-00-00 00:00:00', 0),
-(8, '', 0, '0000-00-00 00:00:00', 0),
-(9, 'dad', 0, '1999-03-12 00:00:00', 0),
-(10, 'dad', 0, '1999-03-12 00:00:00', 0),
-(11, 'dad', 0, '1999-03-12 00:00:00', 0),
-(12, '', 0, '0000-00-00 00:00:00', 1),
-(13, 'dsad', 0, '1111-01-11 00:00:00', 1),
-(14, 'das', 0, '1223-02-11 00:00:00', 0),
-(15, 'das', 0, '1223-02-11 00:00:00', 0),
-(16, 'dad', 0, '3131-02-11 00:00:00', 2),
-(17, 'dad', 0, '3131-02-11 00:00:00', 2),
-(18, 'dsa', 0, '0000-00-00 00:00:00', 2),
-(19, 'dsa', 0, '0000-00-00 00:00:00', 2),
-(20, 'dsa', 0, '0000-00-00 00:00:00', 2),
-(21, 'dsa', 0, '0000-00-00 00:00:00', 2);
+INSERT INTO `bookingform` (`booking_id`, `user_id`, `booking_fullname`, `booking_contact`, `booking_date`, `booking_time`, `numberPerson`) VALUES
+(1, 1, 'YAM ZHENG LIM', '0129747749', '2021-04-10', '17:16:15', 2);
 
 -- --------------------------------------------------------
 
@@ -69,9 +51,10 @@ INSERT INTO `bookingform` (`id`, `customer_name`, `contact`, `datentime`, `peopl
 --
 
 CREATE TABLE `users` (
-  `id` int(11) NOT NULL,
+  `user_id` int(11) NOT NULL,
   `username` varchar(50) NOT NULL,
   `password` varchar(255) NOT NULL,
+  `user_fullname` varchar(50) NOT NULL,
   `role` varchar(12) NOT NULL DEFAULT 'user',
   `created_at` datetime DEFAULT current_timestamp()
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
@@ -80,10 +63,10 @@ CREATE TABLE `users` (
 -- Dumping data for table `users`
 --
 
-INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`) VALUES
-(1, 'zhenglim99', '$2y$10$DcjO5izJIxKmjYdQ3qioAeiz5EpLUU7lAUHpM0QYMfBoKJeC46vk6', 'user', '2021-04-06 21:20:29'),
-(3, 'Lik Wei', '$2y$10$IeleHC7AfOJ4zNz3OAJ2RuhBxGZOHcORDDVMs4hOn8sNp7F1jE4Ki', 'user', '2021-04-10 09:16:21'),
-(4, 'admin', '$2y$10$hZ5SbIX.jIRqqGYuVgxw3.ZCm0NNZp3MnWk3o3bl3QoanlFwIDDFu', 'user', '2021-04-10 11:19:15');
+INSERT INTO `users` (`user_id`, `username`, `password`, `user_fullname`, `role`, `created_at`) VALUES
+(1, 'zhenglim99', '$2y$10$DcjO5izJIxKmjYdQ3qioAeiz5EpLUU7lAUHpM0QYMfBoKJeC46vk6', '0', 'user', '2021-04-06 21:20:29'),
+(3, 'Lik Wei', '$2y$10$IeleHC7AfOJ4zNz3OAJ2RuhBxGZOHcORDDVMs4hOn8sNp7F1jE4Ki', '0', 'user', '2021-04-10 09:16:21'),
+(4, 'admin', '$2y$10$hZ5SbIX.jIRqqGYuVgxw3.ZCm0NNZp3MnWk3o3bl3QoanlFwIDDFu', '0', 'user', '2021-04-10 11:19:15');
 
 --
 -- Indexes for dumped tables
@@ -93,13 +76,14 @@ INSERT INTO `users` (`id`, `username`, `password`, `role`, `created_at`) VALUES
 -- Indexes for table `bookingform`
 --
 ALTER TABLE `bookingform`
-  ADD PRIMARY KEY (`id`);
+  ADD PRIMARY KEY (`booking_id`),
+  ADD KEY `user_id` (`user_id`);
 
 --
 -- Indexes for table `users`
 --
 ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
+  ADD PRIMARY KEY (`user_id`),
   ADD UNIQUE KEY `username` (`username`);
 
 --
@@ -110,13 +94,23 @@ ALTER TABLE `users`
 -- AUTO_INCREMENT for table `bookingform`
 --
 ALTER TABLE `bookingform`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=22;
+  MODIFY `booking_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 
 --
 -- AUTO_INCREMENT for table `users`
 --
 ALTER TABLE `users`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+  MODIFY `user_id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=5;
+
+--
+-- Constraints for dumped tables
+--
+
+--
+-- Constraints for table `bookingform`
+--
+ALTER TABLE `bookingform`
+  ADD CONSTRAINT `bookingform_ibfk_1` FOREIGN KEY (`user_id`) REFERENCES `users` (`user_id`);
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
