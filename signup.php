18bb95b0061080ac682fd6d4b8b63a0530f8c99d
likweitan <likweitan@gmail.com>
2021-04-10 18:12:58 +0800

    Fixed signup

--- signup.php
+++ signup.php
@@ -80,10 +80,11 @@ if($_SERVER["REQUEST_METHOD"] == "POST"){
         
         // Prepare an insert statement
         $sql = "INSERT INTO users (username, password, user_fullname) VALUES (?, ?, ?)";
-         
+        echo $fullname;
+
         if($stmt = mysqli_prepare($link, $sql)){
             // Bind variables to the prepared statement as parameters
-            mysqli_stmt_bind_param($stmt, "ss", $param_username, $param_password, $param_fullname);
+            mysqli_stmt_bind_param($stmt,'sss', $param_username, $param_password, $param_fullname);
             
             // Set parameters
             $param_fullname = $fullname;
