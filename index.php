c32f6953c5ecafcf22f8a7a62763b64f2321dde3
likweitan <likweitan@gmail.com>
2021-04-10 11:30:03 +0800

    Redirect role to particular pages

--- index.php
+++ index.php
@@ -1,11 +1,30 @@
 <?php
+require("config.php");
 // Initialize the session
 session_start();
- 
+
+if(isset($_SESSION['username']))
+{
+    $sql = "SELECT *
+            FROM users
+            WHERE username =".$_SESSION['username'];
+    $query = mysqli_query($link,$sql);
+    if($query)
+    {
+        while($row = mysqli_fetch_array($query))
+        {
+            $myRole = $row['role'];
+        }
+    }
+}
+
 // Check if the user is logged in, if not then redirect him to login page
 if(!isset($_SESSION["loggedin"]) || $_SESSION["loggedin"] !== true){
     header("location: login.php");
     exit;
+}
+else if($myRole == 'admin'){
+    header("location: admin.php");
 }
 else{
     header("location: welcome.php");
