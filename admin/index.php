704878385a77ae8c521691ebacb3fcf29b4ff3a1
Yam Zheng Lim <yamzhenglim0427@gmail.com>
2021-04-13 23:12:59 +0800

    delete calendar put in calendar.php

--- admin/index.php
+++ admin/index.php
@@ -4,84 +4,9 @@
     if(isset($_SESSION['id']))
     {
         $sql = "SELECT * FROM bookingform
-                #WHERE booking_date >= DATE(CURRENT_TIMESTAMP())
+                WHERE booking_date >= DATE(CURRENT_TIMESTAMP())
                 ORDER BY booking_create_at DESC";
         $query = mysqli_query($link,$sql);
-    };
-
-
-    //calendar PHP
-    // Set your timezone
-    date_default_timezone_set('Asia/Singapore');
-    
-    // Get prev & next month
-    if (isset($_GET['ym'])) {
-        $ym = $_GET['ym'];
-    } else {
-        // This month
-        $ym = date('Y-m');
-    }
-    
-    // Check format
-    $timestamp = strtotime($ym . '-01');
-    if ($timestamp === false) {
-        $ym = date('Y-m');
-        $timestamp = strtotime($ym . '-01');
-    }
-    
-    // Today
-    $today = date('Y-m-j', time());
-    
-    // For H3 title
-    $html_title = date('Y / m', $timestamp);
-    
-    // Create prev & next month link     mktime(hour,minute,second,month,day,year)
-    $prev = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)-1, 1, date('Y', $timestamp)));
-    $next = date('Y-m', mktime(0, 0, 0, date('m', $timestamp)+1, 1, date('Y', $timestamp)));
-    // You can also use strtotime!
-    // $prev = date('Y-m', strtotime('-1 month', $timestamp));
-    // $next = date('Y-m', strtotime('+1 month', $timestamp));
-    
-    // Number of days in the month
-    $day_count = date('t', $timestamp);
-     
-    // 0:Sun 1:Mon 2:Tue ...
-    $str = date('w', mktime(0, 0, 0, date('m', $timestamp), 1, date('Y', $timestamp)));
-    //$str = date('w', $timestamp);
-    
-    
-    // Create Calendar!!
-    $weeks = array();
-    $week = '';
-    
-    // Add empty cell
-    $week .= str_repeat('<td></td>', $str);
-    
-    for ( $day = 1; $day <= $day_count; $day++, $str++) {
-         
-        $date = $ym . '-' . $day;
-         
-        if ($today == $date) {
-            $week .= '<td class="today">' . $day;
-        } else {
-            $week .= '<td>' . $day;
-        }
-        $week .= '</td>';
-         
-        // End of the week OR End of the month
-        if ($str % 7 == 6 || $day == $day_count) {
-    
-            if ($day == $day_count) {
-                // Add empty cell
-                $week .= str_repeat('<td></td>', 6 - ($str % 7));
-            }
-    
-            $weeks[] = '<tr>' . $week . '</tr>';
-    
-            // Prepare for new week
-            $week = '';
-        }
-    
     }
     
 ?>
@@ -117,28 +42,6 @@
           font-size: 3.5rem;
         }
       }
-    </style>
-
-    <!--Calendar CSS & JS -->
-    <link href="https://fonts.googleapis.com/css?family=Noto+Sans" rel="stylesheet">
-    <style>
-        .calendar {
-            font-family: 'Noto Sans', sans-serif;
-            margin-top: 80px;
-        }
-        .calendar h3 {
-            margin-bottom: 30px;
-        }
-        .calendar th {
-            height: 30px;
-            text-align: center;
-        }
-        .calendar td {
-            height: 100px;
-        }
-        .calendar .today {
-            background: orange;
-        }
     </style>
 
     <!-- Custom styles for this template -->
@@ -191,30 +94,6 @@
     </nav>
 
     <main class="col-md-9 ms-sm-auto col-lg-10 px-md-4">
-
-        <!--calender test start -->
-        
-        <div class="calendar">
-        <h3><a href="?ym=<?php echo $prev; ?>">&lt;</a> <?php echo $html_title; ?> <a href="?ym=<?php echo $next; ?>">&gt;</a></h3>
-        <table class="table table-bordered">
-            <tr>
-                <th>S</th>
-                <th>M</th>
-                <th>T</th>
-                <th>W</th>
-                <th>T</th>
-                <th>F</th>
-                <th>S</th>
-            </tr>
-            <?php
-                foreach ($weeks as $week) {
-                    echo $week;
-                }
-            ?>
-        </table>
-    </div>
-
-        <!--calender test end -->
       
       <div class="d-flex justify-content-between flex-wrap flex-md-nowrap align-items-center pt-3 pb-2 mb-3 border-bottom">   
         <h1 class="h2">Bookings</h1>
