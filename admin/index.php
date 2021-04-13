ab69ddc4e55a78200ecc743b02a2212806065ce8
likweitan <likweitan@gmail.com>
2021-04-13 21:55:58 +0800

    Set title

--- admin/index.php
+++ admin/index.php
@@ -94,7 +94,7 @@
     <meta name="description" content="">
     <meta name="author" content="Mark Otto, Jacob Thornton, and Bootstrap contributors">
     <meta name="generator" content="Hugo 0.82.0">
-    <title>Dashboard Template · Bootstrap v5.0</title>
+    <title>Admin Dashboard - The Muffin House</title>
 
     <link rel="canonical" href="https://getbootstrap.com/docs/5.0/examples/dashboard/">
 
