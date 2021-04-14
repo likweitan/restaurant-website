6698bc95dd48bea58e6896db1f0475ad4b68a3a1
Yam Zheng Lim <yamzhenglim0427@gmail.com>
2021-04-14 13:19:18 +0800

    bug fix

--- admin/index.php
+++ admin/index.php
@@ -54,7 +54,7 @@
   <body>
     
 <header class="navbar navbar-dark sticky-top flex-md-nowrap p-0 shadow" style="background-color:rgba(26, 24, 22, 0.8)">
-  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" style ="font-family: 'Satisfy', sans-serif; font-size: 1.5em;" href="#">The Muffin House</a>
+  <a class="navbar-brand col-md-3 col-lg-2 me-0 px-3" style ="font-family: 'Satisfy', sans-serif; font-size: 1.5em;" href="../welcome.php">The Muffin House</a>
   <button class="navbar-toggler position-absolute d-md-none collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#sidebarMenu" aria-controls="sidebarMenu" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
